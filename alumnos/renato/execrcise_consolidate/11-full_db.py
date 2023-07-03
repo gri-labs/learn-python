b932721e11dd6ba75544bce671a035cc2d9a77fb
Renato Acciardi <a.renato@gmail.com>
2023-07-03 19:51:30 +0200

    WIP

---
+++ alumnos/renato/execrcise_consolidate/11-full_db.py
@@ -0,0 +1,104 @@
+# Genera un script que pueda crear una base de datos de estudiantes
+# con una tabla llamada estudiantes que tenga dos campos.
+# Inserta un registro en la tabla estudiantes con dos campos.
+# Consulta todos los registros de la tabla estudiantes y muéstralos en pantalla.
+# Consulta un registro de la tabla estudiantes y muéstralo en pantalla.
+# Cada ejecución del script tiene que estar en funciones separadas
+
+import logging
+import mysql.connector
+
+
+def connection_database(database_name):
+    connection = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='root',
+        port=int(3307),
+        database=database_name,
+    )
+    connection.connect()
+    return connection
+
+
+def create_db(db_name):
+    create_database_sql = f"""
+    CREATE DATABASE IF NOT EXISTS {db_name};
+    """
+    return create_database_sql
+
+
+def create_table(db_name, table_name):
+    create_tbl_sql = f"""
+    CREATE TABLE IF NOT EXISTS {db_name}.{table_name} (
+    id INT,
+    nombre VARCHAR(25),
+    carrera VARCHAR(255)
+    );
+    """
+    return create_tbl_sql
+
+
+def insert_data(tabla, id, name, career):
+    insert_sql = f"""
+    INSERT INTO {tabla} (id, nombre, carrera)
+    VALUES ('{id}', '{name}', '{career}')
+    """
+    return insert_sql
+
+
+def get_data(name_db):
+    select_sql = f"""
+    SELECT * FROM {name_db}
+    """
+    return select_sql
+
+
+def get_data_by_id(name_db, id):
+    select_sql = f"""
+    SELECT * FROM {name_db} WHERE id = {id}
+    """
+    return select_sql
+
+
+def exec_and_commit(cursor, connection, query):
+    cursor.execute(query)
+    connection.commit()
+
+
+def exec(cursor, query):
+    cursor.execute(query)
+
+
+# Arranque del servidor o inicio del programa
+if __name__ == '__main__':
+    # Se configura el log
+    logging.basicConfig(filename='request.log', level=logging.DEBUG)
+
+    # Creo DB MyNewDb in MySQL
+    connection_db = connection_database('mysql')
+    cursor_db = connection_db.cursor()
+    create_db_sql = create_db("MyNewDb")
+    exec_and_commit(cursor_db, connection_db, create_db_sql)
+
+    create_table_sql = create_table("MyNewDb", "NewEstudiantes")
+    exec_and_commit(cursor_db, connection_db, create_table_sql)
+    cursor_db.close()
+    connection_db.close()
+
+    connection_db = connection_database('MyNewDb')
+    cursor_db = connection_db.cursor()
+    insert_data_sql = insert_data("NewEstudiantes", 1, "Ginger", "Gato")
+    exec_and_commit(cursor_db, connection_db, insert_data_sql)
+
+    get_all_sql = get_data("NewEstudiantes")
+    exec(cursor_db, get_all_sql)
+    rows = cursor_db.fetchall()
+    for row in rows:
+        print(row)
+
+    get_by_id_sql = get_data_by_id("NewEstudiantes", 1)
+    exec(cursor_db, get_by_id_sql)
+    rows = cursor_db.fetchall()
+    for row in rows:
+        print(row)
--- alumnos/renato/execrcise_consolidate/12-db_and_backend.py
+++ alumnos/renato/execrcise_consolidate/12-db_and_backend.py
@@ -1,9 +1,10 @@
-# Importa la clase flask del modulo flask
-# Flask es una clase que nos permite crear una aplicación web
-# Flask es un framework
-# Un framework es un conjunto de herramientas que nos permite desarrollar aplicaciones
+# Siguiendo el ejemplo del script app y el script full_db, crea un script que:
+# tenga un endpoint que muestre todos los registros de la tabla estudiantes
+# tenga un endpoint que muestre un registro de la tabla estudiantes
+# tenga un endpoint que inserte un registro en la tabla estudiantes
+# tenga un endpoint que elimine un registro de la tabla estudiantes
 from flask import Flask
-# Importa el modulo logging
+import mysql.connector
 import logging
 
 # Se crea una instancia de la clase flask llamada app
@@ -11,18 +12,21 @@ import logging
 app = Flask(__name__)
 
 
+def connection_database():
+
+
+def execute_query():
+
+
 # Se define una ruta para la aplicación
 # La ruta es la raíz de la aplicación
 @app.route('/', methods=['GET'])
 # Define una función llamada hell_world
 # Nos ayuda a encapsular el código, mantener funcionalidades...
-def hello_world():
-    return 'Hello World!'
+def get_estudiantes():
 
 
-# Arranque del servidor o inicio del programa
 if __name__ == '__main__':
     # Se configura el log
     logging.basicConfig(filename='request.log', level=logging.DEBUG)
-    # Se ejecuta el servidor
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=6000)
