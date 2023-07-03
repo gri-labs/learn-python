95a9981c2f438f30becc63da681e4925665714b6
naracasaes <naracasaes@hotmail.com>
2023-07-03 18:57:19 +0200

    WIP - 11

---
+++ alumnos/Itamara/execrcise_consolidate/11-full_db.py
@@ -0,0 +1,30 @@
+# Genera un script que pueda crear una base de datos de estudiantes
+# con una tabla llamada estudiantes que tenga dos campos.
+# Inserta un registro en la tabla estudiantes con dos campos.
+# Consulta todos los registros de la tabla estudiantes y muéstralos en pantalla.
+# Consulta un registro de la tabla estudiantes y muéstralo en pantalla.
+# Cada ejecución del script tiene que estar en funciones separadas
+
+import mysql.connector
+def connection_database(host, user, password, database):
+    connection = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='root',
+        port=int(3307),
+        database="mysql"
+    )
+    return connection
+
+def create_table(table):
+    table="CREATE DATABASE IF NOT EXISTS coches;"
+
+
+def insert_data():
+
+def get_data():
+
+def get_data_by_id():
+
+
+if __name__ == '__main__':
