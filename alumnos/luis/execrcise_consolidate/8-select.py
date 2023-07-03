ea6536a23c0a3a956c34cc155ebadd7e7d39dc03
Luis Carbonell <luiscarbonell20gmail.com>
2023-07-03 18:38:40 +0200

    ejercicios python

---
+++ alumnos/luis/execrcise_consolidate/8-select.py
@@ -0,0 +1,29 @@
+import mysql.connector
+
+connection = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='root',
+    database=mysql
+)
+
+# Crear un cursor para ejecutar sentencias SQL
+cursor = connection.cursor()
+
+# Consultar todos los registros
+select_sql = """
+SELECT * FROM estudiantes
+"""
+
+cursor.execute(select_sql)
+
+# Obtener los resultados
+rows = cursor.fetchall()
+
+# Mostrar los resultados
+for row in rows:
+    print(row)
+
+# Cerrar el cursor y la conexión
+cursor.close()
+connection.close()
