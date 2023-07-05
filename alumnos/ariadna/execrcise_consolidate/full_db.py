533fc525a40be273fd511a1f474c3931abf86c4f
AriadnaLlB <ariadna.lb@outlook.com>
2023-07-05 19:59:40 +0200

    add fulldb

--- alumnos/ariadna/execrcise_consolidate/full_db.py
+++ alumnos/ariadna/execrcise_consolidate/full_db.py
@@ -8,7 +8,7 @@
 import mysql.connector
 
 
-def connection_database(host,user,password,database,port):
+def connection_database(host, user, password, database, port):
     connection = mysql.connector.connect(
         host=host,
         user=user,
@@ -20,32 +20,38 @@ def connection_database(host,user,password,database,port):
     return connection
 
 
-def create_database(connect):
+def create_database(connection):
     query = "CREATE DATABASE IF NOT EXISTS estudiantes;"
-    commit_query(connect, query)
+    commit_query(connection, query)
 
 
-def create_table(connect):
+def create_table(connection):
     query = "CREATE TABLE IF NOT EXISTS `estudiantes`.`estudiantes` (id INT, nombre VARCHAR(255));"
-    commit_query(connect, query)
+    commit_query(connection, query)
 
 
-def insert_data(connect):
-    query = ""
-    commit_query(connect, query)
+def insert_data(connection):
+    query = "INSERT INTO estudiantes (id, nombre) VALUES (1, 'ari');"
+    commit_query(connection, query)
 
 
-def get_data(connect):
+def get_data(connection):
     query = "SELECT * FROM estudiantes"
-    return execute_query_select(connect, query)
+    return execute_query_select(connection, query)
 
 
 def commit_query(connection, query):
+    cursor = connection.cursor()
+    cursor.execute(query)
+    connection.commit()
+    cursor.close()
 
 
 def execute_query_select(connection, query):
-
-
+    cursor = connection.cursor()
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    return rows
 
 
 def print_data(rows):
@@ -58,10 +64,10 @@ def close_connection(connection):
 
 
 if __name__ == '__main__':
-    conn = connection_database()
-    create_database()
-    create_table()
-    insert_data()
-    data = get_data()
-    print_data()
-    close_connection()
+    conn = connection_database("localhost", "root", "root", "estudiantes", 3307)
+    create_database(conn)
+    create_table(conn)
+    insert_data(conn)
+    data = get_data(conn)
+    print_data(data)
+    close_connection(conn)
