0e0c1fdbcaf4bdb8dc059b8435c211bdb87d85df
AliLlanos <alhidaburitica@gmail.com>
2023-07-05 18:31:48 +0200

    add exercises 8,10,11 of consolidate/alhida

---
+++ alumnos/Alhida/exercise_consolidate/10-delete.py
@@ -0,0 +1,26 @@
+import mysql.connector
+
+connection = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='root',
+    port=int(3307),
+    database='estudiantes',
+
+)
+
+# Crear un cursor para ejecutar sentencias SQL
+cursor= connection.cursor()
+
+# Consultar todos los registros
+delete_sql = "DELETE FROM * estudiantes WHERE id=1"
+
+# Ejecutar los cambios en la base de datos
+cursor.execute(delete_sql)
+
+# Confirmar los cambios en la base de datos
+connection.commit()
+
+# Cerrar el cursor y la conexión
+cursor.close()
+connection.close()
