c4ac6aa8742ee20de8cb9187c8a472c3f4c1cfd5
naracasaes <naracasaes@hotmail.com>
2023-07-07 19:01:40 +0200

    WIP

--- alumnos/Itamara/execrcise_consolidate/12-complet_db_and_backend.py
+++ alumnos/Itamara/execrcise_consolidate/12-complet_db_and_backend.py
@@ -6,114 +6,89 @@
 import Flask from flask, request, jasonfy
 import mysql.connector
 
-
-
 app = Flask(__name__)
 
 
-def connection_database():
+def connection_database(host, user, password, database, port):
     connection = mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='root',
-        database='gri',
-        port=int(3307),
+        host=host,
+        user=user,
+        password=password,
+        database=database,
+        port=int(port),
     )
-
-    return connection
+    return jsonify(connection)
 
 
 def execute_query(connection, query):
-    cursor = connection.cursor()
-    cursor.execute(query)
-    result = cursor.fetchall()
-    cursor.close()
-    return result
+    return jsonify('TODO implementar')
+
+
+def execute_query_with_commit(connection, query):
+    return jsonify('TODO implementar')
+
+
+def connection_close(connection):
+    connection.close()
 
 
 # curl http://localhost:6000/students
+# Usar el --head para ver la respuesta del servidor
 # Metodo GET
 # Es útil para obtener información
 @app.route('/students', methods=['GET'], endpoint='get_all_students')
 def get_all_students():
-    query = "SELECT * FROM estudiantes;"
     connection = connection_database()
+    query = "SELECT * FROM estudiantes;"
     result = execute_query(connection, query)
-    connection.close()
-
-    if len(result) == 0:
-        # devolver un 404
-        return jsonify('No se ha encontrado el estudiante'), 404
-
-    return jsonfy(result)
+    return str(result)
 
 
 # curl http://localhost:6000/student/1
-# Metodo GET con parametros en la URL
+# Usar el --head para ver la respuesta del servidor
+# Método GET con parámetros en la URL
 # Es útil para obtener información de un registro en concreto
+# Que significa el <int:student_id>?
+# Es un parámetro que se pasa en la URL y que se puede usar en el método
+# En este caso se llama student_id y es un entero
 @app.route('/student/<int:student_id>', methods=['GET'], endpoint='get_student')
 def get_student(student_id):
-    query = "SELECT * FROM estudiantes WHERE id = %s;" % student_id
-    connection = connection_database()
-    result = execute_query(connection, query)
-    if len(result) == 0:
-        # devolver un 404
-        return jsonify('No se ha encontrado el estudiante'), 404
-    connection.close()
-
-    return str(result)
+    return jsonify("TODO implementar")
 
 
 # curl -X POST http://localhost:6000/student/1/Ricardo
-# Metodo POST con parametros en la URL
+# En este método POST se envía la información en la URL con dos parametros /1/Ricardo
+# Es útil para insertar poca información
 @app.route('/student/<int:student_id>/<string:student_name>', methods=['POST'], endpoint='insert_student')
 def insert_student(student_id, student_name):
-    query = "INSERT INTO estudiantes (id, nombre) VALUES (%s, '%s');" % (student_id, student_name)
-    connection = connection_database()
-    result = execute_query(connection, query)
-    connection.close()
-    return str(result)
+    return jsonify('OK')
 
 
-# curl -X POST http://localhost:6000/student/headers -H 'student_id: 1' -H 'student_name: Ricardo'
-# Metodo POST con headers
-# Que son los headers?
-# Es información que se envía en la cabecera de la petición
-# forma parte del protocolo HTTP
-# Se envían en el formato clave: valor
-# Es útil para enviar información que no se quiere que se vea en la URL
-# Es útil para enviar mucha información
+# curl -X POST "http://localhost:6000/student/headers?student_id=1&student_name=ricardo"
+# Método POST con parámetros en la URL
+# Es útil para insertar información
 @app.route('/student/headers', methods=['POST'], endpoint='insert_student_headers')
 def insert_student_headers():
-    student_id = request.headers.get('student_id')
-    student_name = request.headers.get('student_name')
-    query = "INSERT INTO estudiantes (id, nombre) VALUES (%s, '%s');" % (student_id, student_name)
-    connection = connection_database()
-    result = execute_query(connection, query)
-    connection.close()
-    return str(result)
+    student_id = request.args.get('student_id')
+    student_name = request.args.get('student_name')
+    return jsonify('TODO implementar')
+
 
 # curl -X DELETE http://localhost:6000/student/1
 # Metodo DELETE
 # Es útil para eliminar información
 @app.route('/student/<int:student_id>', methods=['DELETE'], endpoint='delete_student')
 def delete_student(student_id):
-    query = "DELETE FROM estudiantes WHERE id %s;" % student_id
-    connection = connection_database()
-    result = execute_query(connection, query)
-    connection.close()
-    return str(result)
+    return jsonify('TODO implementar')
 
-# curl -X PUT http://localhost:6000/student/1/Ricardo
+
+# curl -X PUT http://localhost:6000/student/1/pepe
 # Metodo PUT
 # Es útil para actualizar información
 @app.route('/student/<int:student_id>/<string:student_name>', methods=['PUT'], endpoint='update_student')
 def update_student(student_id, student_name):
-    query = "UPDATE estudiantes SET nombre = '%s' WHERE id = %s;" % (student_name, student_id)
-    connection = connection_database()
-    result = execute_query(connection, query)
-    connection.close()
-    return str(result)
+    query = "UPDATE estudiantes SET nombre = '%s' WHERE id=%s;" % (student_name, student_id)
+    return jsonify('TODO implementar')
 
 
 if __name__ == '__main__':
---
+++ alumnos/Itamara/execrcise_consolidate/README.md
@@ -0,0 +1,28 @@
+API - Application Programming Interface - Interfaz de Programación de Aplicaciones
+
+
+API (Flask) - Routes - Rutas
+-----------------------------
+"/" - Home
+"/api/v1.0/students" - Students
+"/api/v1.0/students/<int:student_id>" - Student
+
+
+API (Flask) - Methods - Métodos
+-------------------------------
+GET - Obtener
+POST - Crear
+PUT - Actualizar
+DELETE - Borrar
+
+Backend - Servidor
+------------------
+
+Métodos
+-------
+get_students()
+get_student(student_id)
+create_student(student)
+update_student(student_id, student)
+delete_student(student_id)
+insert_student(student)
