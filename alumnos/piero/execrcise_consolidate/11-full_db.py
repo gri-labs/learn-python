0fa2c6327e2bb168ed8135516b038c7862a6a9e4
Piero Giorgio <pierogiorgiooo@gmail.com>
2023-07-10 19:58:12 +0200

    ejercicio11_revision

--- alumnos/piero/execrcise_consolidate/11-full_db.py
+++ alumnos/piero/execrcise_consolidate/11-full_db.py
@@ -7,54 +7,46 @@
 
 import mysql.connector
 
-def connection_database():
+def connection_database(host, user, password, database):
     connection = mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='root',
+        host=host,
+        user=user,
+        password=password,
         port=int(3307),
-        database='estudiantes'
+        database=database
 )
     return connection
 
-def create_database(connection):
+def cursor_commit_query(connection, query):
     cursor = connection.cursor()
-    create_database_sql = "CREATE DATABASE IF NOT EXISTS estudiantes"
-    cursor.execute(create_database_sql)
-    cursor.close
-
-def create_table(connection):
-    cursor = connection.cursor()
-    create_table_sql = "CREATE TABLE IF NOT EXISTS estudiante (id INT NOT NULL,nombre VARCHAR(100),carrera VARCHAR(100))"
-    cursor.execute(create_table_sql)
-    cursor.close
-
-
-def insert_data(connection):
-    cursor = connection.cursor()
-    insert_sql = "INSERT INTO estudiante (id, nombre, carrera) VALUES (1, 'Piero', 'Profe')"
-    cursor.execute(insert_sql)
+    cursor.execute(query)
     connection.commit()
-    cursor.close
-
-
-def get_data(connection):
-    cursor = connection.cursor()
-    select_sql = "SELECT * FROM estudiante"
-    cursor.execute(select_sql)
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
-    cursor.close
-
-def get_data_by_id(connection):
-    cursor = connection.cursor()
-    select_sql = "SELECT * FROM estudiante WHERE id = 1"
-    cursor.execute(select_sql)
-    result = cursor.fetchone()
-    print(result)
     cursor.close()
 
+def execute_query(query, connection):
+    cursor = connection.cursor()
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    cursor.close()
+    return rows
+
+def create_database(connection):
+    cursor_commit_query(connection, "CREATE DATABASE IF NOT EXISTS estudiantes")
+
+def create_table(connection):
+    cursor_commit_query(connection, "CREATE TABLE IF NOT EXISTS estudiante (id INT NOT NULL,nombre VARCHAR(100),carrera VARCHAR(100))")
+
+def insert_data(connection):
+    cursor_commit_query(connection, "INSERT INTO estudiante (id, nombre, carrera) VALUES (3, 'Thor', 'HijoDeOdin')")
+
+def get_data(connection):
+    rows = execute_query("SELECT * FROM estudiante", connection)
+    for row in rows:
+        print(row)
+
+def get_data_by_id(connection):
+    row = execute_query("SELECT * FROM estudiante WHERE id = 1", connection)
+    print(row)
 
 if __name__ == '__main__':
     conn = connection_database("localhost", "root", "root", "estudiantes")
