e584199f1db513b1dee45ef06191f1c55ab2a09e
adrianjsg <adrianjsg87@gmail.com>
2023-07-11 18:53:37 +0200

    A ver si el 11 se ve ya

--- alumnos/Adrian/11-full_db.py
+++ alumnos/Adrian/11-full_db.py
@@ -20,17 +20,17 @@ def connection_database():
 
 
 def create_database(connect):
-    query = ""
+    query = "CREATE DATABASE IF NOT EXISTS gri;"
     commit_query(connect, query)
 
 
 def create_table(connect):
-    query = ""
+    query = "CREATE TABLE IF NOT EXISTS 'gri'.'ESTUDIANTES'(id INT, nombre VARCHAR(255), edad INT)"
     commit_query(connect, query)
 
 
 def insert_data(connect):
-    query = ""
+    query = "INSERT INTO estudiantes (nombre, edad) VALUES ('Pepe',33);"
     commit_query(connect, query)
 
 
@@ -38,11 +38,24 @@ def get_data(connect):
     query = "SELECT * FROM `gri`.`estudiantes`;"
     return execute_query_select(connect, query)
 
+def get_single_data(connect):
+    query = "SELECT * FROM estudiantes WHERE id = 1;"
+    return execute_query_select(connect, query)
+
 
 def commit_query(connection, query):
+    cursor = connection.cursor()
+    cursor.execute(query)
+    connection.commit()
+    cursor.close()
 
 
 def execute_query_select(connection, query):
+    cursor = connection.cursor()
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    cursor.close()
+    return rows
 
 
 def print_data(rows):
@@ -55,10 +68,12 @@ def close_connection(connection):
 
 
 if __name__ == '__main__':
-    conn = connection_database()
-    create_database()
-    create_table()
-    insert_data()
-    data = get_data()
-    print_data()
-    close_connection()
+    conn = connection_database('localhost','root','root','gri',3308)
+    create_database(conn)
+    create_table(conn)
+    insert_data(conn)
+    data = get_data(conn)
+    print_data(data)
+    single_data = get_single_data(conn)
+    print_data(single_data)
+    close_connection(conn)
