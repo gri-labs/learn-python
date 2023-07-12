71b79b3950faedf156213f34bf4da9e9c16d80a0
Renato <acciardi.dev@gmail.com>
2023-07-12 19:20:56 +0200

    first commit

---
+++ alumnos/renato/exercise_insert/main.py
@@ -0,0 +1,54 @@
+import mysql.connector
+from flask import jsonify
+import random
+
+
+def connection_database(database_name):
+    connection = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='root',
+        port=int(3307),
+        database=database_name,
+    )
+    return connection
+
+
+def exec_and_commit(query):
+    connection_db = connection_database('usuarios')
+    cursor = connection_db.cursor()
+    cursor.execute(query)
+    connection_db.commit()
+    cursor.close()
+    connection_db.close()
+
+
+def insert_data():
+    nombres = ['Renato', 'Loida', 'Merlin', 'Ginger', 'Nata']
+    emails = ['renato@gmail.com', 'loida@hotmail.com', 'Merlin@cat.com', 'Ginger@cat1.com', 'Nata@cat2.com']
+    appellidos = ['Pepe', 'Pedro', 'Juanjo', 'Gimmy', 'Sanchez']
+
+    for i in range(10000):
+        user_name = random.choice(nombres)+str(random.randint(0, 1000))
+        email = str(random.randint(0, 1000))+random.choice(emails)
+        appellido = random.choice(appellidos)+" "+str(random.randint(0, 1000))
+        edad = random.randint(18, 60)
+
+        sql_code = f"""
+        INSERT INTO usuarios.users (nombre, email, appellido, edad)
+        VALUES ('{user_name}', '{email}',  '{appellido}', '{edad}')
+        """
+        exec_and_commit(sql_code)
+        return jsonify("OK"), 201
+
+
+def delete_data():
+    pass
+
+
+if __name__ == '__main__':
+    name = input("Which action? ")
+    if name.upper() == "INSERT":
+        insert_data()
+    elif name.upper() == "DELETE":
+        delete_data()
