760e3aafafd23598fde2ed8823aeaa44ca7997e7
Piero Giorgio <pierogiorgiooo@gmail.com>
2023-07-13 19:50:19 +0200

    script_bd_new

--- alumnos/piero/ejercicios_bd/script_bd.py
+++ alumnos/piero/ejercicios_bd/script_bd.py
@@ -2,8 +2,6 @@ import random
 from faker import Faker
 import mysql.connector
 
-fake = Faker(['it_IT', 'es_ES', 'en_US'])
-
 
 def connection_database(host, user, password, database):
     connection = mysql.connector.connect(
@@ -31,76 +29,73 @@ def execute_query(query, connection):
     return rows
 
 
-def gen_correo_electronico(nombre, apellido):
-    proveedor = fake.free_email_domain()
-    correo_electronico = f"{nombre}.{apellido}@{proveedor}"
-    return correo_electronico
+class DBOperator:
+    def __init__(self, connection):
+        self.connection = connection
+        self.fake = Faker(['it_IT', 'es_ES', 'en_US'])
 
+    def gen_correo_electronico(self, nombre, apellido):
+        proveedor = self.fake.free_email_domain()
+        correo_electronico = f"{nombre}.{apellido}@{proveedor}"
+        return correo_electronico
 
-def gen_nombre():
-    nombre = fake.first_name()
-    return nombre
+    def gen_nombre(self):
+        nombre = self.fake.first_name()
+        return nombre
 
+    def gen_apellido(self):
+        apellido = self.fake.last_name()
+        return apellido
 
-def gen_apellido():
-    apellido = fake.last_name()
-    return apellido
+    def gen_edad(self):
+        edad = random.randint(18, 65)
+        return edad
 
+    def insert_data(self):
+        for i in range(1, 100000):
+            correo_electronico = self.gen_correo_electronico(self.gen_nombre(), self.gen_apellido())
+            nombre = self.gen_nombre()
+            apellido = self.gen_apellido()
+            edad = self.gen_edad()
+            cursor_commit_query(self.connection, f"INSERT INTO usuarios (email, nombre, apellido, edad) VALUES ('{correo_electronico}', '{nombre}', '{apellido}', {edad})")
 
-def gen_edad():
-    edad = random.randint(18, 65)
-    return edad
+    def delete_data(self):
+        cursor_commit_query(self.connection, "DELETE FROM usuarios")
 
+    def get_data(self):
+        rows = execute_query("SELECT * FROM usuarios", self.connection)
+        if len(rows) == 0:
+            print("No hay registros")
+        else:
+            for row in rows:
+                print(row)
 
-def insert_data(connection):
-    for i in range(1, 100000):
-        correo_electronico = gen_correo_electronico(gen_nombre(), gen_apellido())
-        nombre = gen_nombre()
-        apellido = gen_apellido()
-        edad = gen_edad()
-        cursor_commit_query(connection, f"INSERT INTO usuarios (email, nombre, apellido, edad) VALUES ('{correo_electronico}', '{nombre}', '{apellido}', {edad})")
-
-
-def delete_data(connection):
-    cursor_commit_query(connection, "DELETE FROM usuarios")
-
-
-def get_data(connection):
-    rows = execute_query("SELECT * FROM usuarios", connection)
-    if len(rows) == 0:
-        print("No hay registros")
-    else:
-        for row in rows:
+    def get_data_by_id(self):
+        id = int(input("Indica qué ID quieres buscar: "))
+        row = execute_query(f"SELECT * FROM usuarios WHERE id = {id}", self.connection)
+        if len(row) == 0:
+            print("ID no encontrado")
+        else:
             print(row)
 
+    def run_everything(self):
+        answer = input("Indica si quieres hacer INSERT, DELETE, SELECT o SELECTID: ").lower()
+        while answer not in ["insert", "delete", "select", "selectid"]:
+            print("Debes especificar una de las cuatro opciones")
+            answer = input("Indica si quieres hacer INSERT, DELETE, SELECT o SELECTID: ").lower()
 
-def get_data_by_id(connection):
-    id= int(input(" indica que id quieres buscar:"))
-    row = execute_query(f"SELECT * FROM usuarios WHERE id = {id}", connection)
-    if len(row) == 0:
-        print("ID no encontrado")
-    else:
-        print(row)
-
-
-def run_everything(connection):
-    answer = input("Indicar si quieres hacer INSERT, DELETE, SELECT o SELECTID: ").lower()
-    while answer != "insert" and answer != "delete" and answer != "select" and answer != "selectid":
-        print("Debes especificar una de las cuatro opciones")
-        answer = input("Indicar si quieres hacer INSERT, DELETE, SELECT o SELECTID: ").lower()
-
-    if answer == "insert":
-        insert_data(connection)
-    elif answer == "select":
-        get_data(connection)
-    elif answer == "selectid":
-        get_data_by_id(connection)
-    else:
-        delete_data(connection)
-        get_data(connection)
-
+        if answer == "insert":
+            self.insert_data()
+        elif answer == "delete":
+            self.delete_data()
+            self.get_data()
+        elif answer == "select":
+            self.get_data()
+        elif answer == "selectid":
+            self.get_data_by_id()
 
 if __name__ == '__main__':
     conn = connection_database("localhost", "root", "root", "personas")
-    run_everything(conn)
+    operator = DBOperator(conn)
+    operator.run_everything()
     conn.close()
