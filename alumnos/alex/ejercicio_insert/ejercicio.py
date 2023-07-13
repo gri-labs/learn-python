60db74f44037a2c7b4fb55d7d35a5f17c89da05f
Alex <larrinaga_climent@hotmail.com>
2023-07-13 18:39:59 +0200

    Fix

--- alumnos/alex/ejercicio_insert/ejercicio.py
+++ alumnos/alex/ejercicio_insert/ejercicio.py
@@ -15,6 +15,13 @@ def execute_query_and_commit(connection, query):
     cursor = connection.cursor()
     cursor.execute(query)
     connection.commit()
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    cursor.execute(query)
+    result = cursor.fetchall()
+    cursor.close()
+    return result
 def insert_datos(connection):
     i = 0
     while i < 10000:
@@ -22,7 +29,16 @@ def insert_datos(connection):
         execute_query_and_commit(connection, query)
         i = i + 1
     connection.cursor.close()
+def quering_id(connection, num):
+    query = "SELECT * FROM estudiantes WHERE id=%s;" % (num)
+    execute_query(connection, query)
 if __name__ == '__main__':
     x = input()
     if x == "INSERT":
         insert_datos(connection_database())
+    if x == "SELECT":
+        y = input()
+        result = quering_id(connection_database(), y)
+        print(str(result))
+
+
