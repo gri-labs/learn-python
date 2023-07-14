950dec7f2ff8a4a345d23033c49941479f592d63
Renato <acciardi.dev@gmail.com>
2023-07-14 18:50:50 +0200

    First basic exercises

--- alumnos/renato/basic/car.py
+++ alumnos/renato/basic/car.py
@@ -1,5 +1,6 @@
 class Car:
     def __init__(self, make, model, year):
+        self.mileage = None
         self.make = make
         self.model = model
         self.year = year
@@ -13,5 +14,5 @@ class Car:
 
 if __name__ == '__main__':
     kit = Car("Renault", "Megane", 2004)
-    kit.mileage = 120000
+    kit.set_mileage(120000)
     print(kit.get_mileage())
