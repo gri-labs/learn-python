63fbb604af3f83d50847283d3bdb7e9ddbc926b5
Wilson Forero <wilsonforerob@gmail.com>
2023-07-18 15:17:35 +0200

    Problema con class/repository/memory/basic/main.py con la funcion delete

--- alumnos/Wilson/repository/memory/basic/main.py
+++ alumnos/Wilson/repository/memory/basic/main.py
@@ -18,6 +18,9 @@ class InMemoryRepository:
     def get(self, id):
         return self.data[id]
 
+    def get_all(self):
+        return self.data
+
     # Actualiza un item de la lista data
     def update(self, id, item):
         self.data[id] = item
@@ -26,6 +29,16 @@ class InMemoryRepository:
     def delete(self, id):
         del self.data[id]
 
+    def size_movies(self):
+        return len(self.data)
+
+
+class PrintMovie:
+    def print_movie(self, item):
+        if isinstance(item, dict):
+            for key, movie in item.items():
+                print(key, movie)
+
 
 if __name__ == '__main__':
     print("Enjoy!")
@@ -33,12 +46,14 @@ if __name__ == '__main__':
     in_memory_repository = InMemoryRepository()
     # TODO: Add 1 movie to the repository with the following data:
     # example
+    movies = {'id': 1, 'name': 'Star Wars'}
+    in_memory_repository.add(movies)
+    movies_tt = [{'id': 2, 'name': 'Star Trek'}, {'id': 3, 'name': 'The Lord of the Rings'}]
+    in_memory_repository.add_all(movies_tt)
     # id: 1, name: Star Wars
     # TODO: Add 3 movies to the repository with the following data:
-    # example
-    # id: 1, name: Star Wars
-    # id: 2, name: Star Trek
     # id: 3, name: The Lord of the Rings
+
     # TODO: Implementa una clase para printar los datos de un item
     # TODO: Print the movie with id 1
     # TODO: Print the movie with id 2
@@ -46,7 +61,19 @@ if __name__ == '__main__':
 
     # TODO: Update the movie with id 2 with the following data:
     # example
+    in_memory_repository.update(1, 'Start Trek: Next Generation')
     # id: 2, name: Star Trek: The Next Generation
     # TODO: Print the movie with id 2
+    impresora = PrintMovie()
+    mov_two = in_memory_repository.get(2)
+    impresora.print_movie(mov_two)
     # TODO: Delete the movie with id 1
+    in_memory_repository.delete(0)
     # TODO: Print all the movies
+    list_movies = in_memory_repository.get_all()
+    print(list_movies)
+    total_peliculas = in_memory_repository.size_movies()
+    print(total_peliculas)
+    for i in range(0, total_peliculas):
+        mov = in_memory_repository.get(i)
+        impresora.print_movie(mov)
