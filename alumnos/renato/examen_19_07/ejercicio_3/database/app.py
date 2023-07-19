e2143281f8199ac0d49929e5e3cc27f0fcdabed7
Renato <acciardi.dev@gmail.com>
2023-07-19 19:49:46 +0200

    Separated files

--- alumnos/renato/examen_19_07/ejercicio_3/database/app.py
+++ alumnos/renato/examen_19_07/ejercicio_3/database/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 
 from repositoryDB import RepositoryDB
+from db_connector import DBConnector
 
 app = Flask(__name__)
 
 repo = RepositoryDB()
+db_conn = DBConnector()
 
 
 def print_data(to_print):
@@ -15,24 +17,24 @@ def print_data(to_print):
 
 @app.route('/create/user/<int:user_id>/<string:name>/<int:age>', methods=['POST'])
 def create_user(user_id, name, age):
-    connection_db = repo.connection_database('localhost', 'root', 'root', 3307, 'mysql')
+    connection_db = db_conn.connection_database('localhost', 'root', 'root', 3307, 'mysql')
     repo.insert_data(connection_db, "examen", "users", user_id, name, age)
-    repo.close_connection(connection_db)
+    db_conn.close_connection(connection_db)
     return 'Usuario creado', 200
 
 
 @app.route('/user/<int:user_id>', methods=['GET'])
 def get_user(user_id):
-    connection_db = repo.connection_database('localhost', 'root', 'root', 3307, 'mysql')
+    connection_db = db_conn.connection_database('localhost', 'root', 'root', 3307, 'mysql')
     data_by_id = repo.get_data_by_id(connection_db, "examen", "users", user_id)
-    repo.close_connection(connection_db)
+    db_conn.close_connection(connection_db)
     print_data(data_by_id)
     return "OK", 200
 
 
 if __name__ == '__main__':
-    connection_db = repo.connection_database('localhost', 'root', 'root', 3307, 'mysql')
-    repo.create_db(connection_db, "examen")
-    repo.create_table(connection_db, "examen", "users")
+    connection_db = db_conn.connection_database('localhost', 'root', 'root', 3307, 'mysql')
+    db_conn.create_db(connection_db, "examen")
+    db_conn.create_table(connection_db, "examen", "users")
 
     app.run()
