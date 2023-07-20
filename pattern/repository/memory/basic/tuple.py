67f8423ba7c10891479d58126c07faa9e83f2518
ricardozd <ricardo.olivera@tangelogames.com>
2023-07-20 19:25:15 +0200

    WIP

--- pattern/repository/memory/basic/tuple.py
+++ pattern/repository/memory/basic/tuple.py
@@ -1,4 +1,6 @@
-from print_data
+from print_data import PrintData
+from repository import InMemoryRepository
+
 if __name__ == '__main__':
     # TODO: Create a new instance of InMemoryRepository
     in_memory_repository = InMemoryRepository()
@@ -6,7 +8,10 @@ if __name__ == '__main__':
     # TODO: Create a new instance of PrintData
     print_data = PrintData()
     # TODO: Add array with multiple tuples to the repository
+    movies = [
+    ]
     # TODO: Print the movies
+
     # TODO: Update the movie with id 2
     # TODO: Print the movies
 
