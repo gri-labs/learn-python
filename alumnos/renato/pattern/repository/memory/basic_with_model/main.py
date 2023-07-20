88f92b55f1df244de5f279998104ad66cfea0d91
Renato <acciardi.dev@gmail.com>
2023-07-20 19:14:47 +0200

    Minor changes

--- alumnos/renato/pattern/repository/memory/basic_with_model/main.py
+++ alumnos/renato/pattern/repository/memory/basic_with_model/main.py
@@ -13,14 +13,17 @@ if __name__ == '__main__':
     ]
     repository.add_many(users_array)
     print_users.print_data(repository.get_all())
+
     update_id = input('Dame el ID para cambiar: ')
     update_name = input('Dame el nuevo nombre: ')
     update_email = input('Dame el nuevo email: ')
     repository.update_one(update_id, [update_name, update_email])
     print_users.print_data(repository.get_all())
+
     delete_id = input('Dame el ID para eliminar: ')
     repository.delete_one(delete_id)
     print_users.print_data(repository.get_all())
+
     delete_all = input('Presiona enter para borrar todo')
     repository.delete_all()
     print_users.print_data(repository.get_all())
