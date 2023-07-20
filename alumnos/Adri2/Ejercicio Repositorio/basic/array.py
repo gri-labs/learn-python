e361db0846e43cfb9762a2049bf87136fabc6cc9
adrianjsg <adrianjsg87@gmail.com>
2023-07-20 19:38:40 +0200

    Archivos array y tuple

--- alumnos/Adri2/Ejercicio Repositorio/basic/array.py
+++ alumnos/Adri2/Ejercicio Repositorio/basic/array.py
@@ -1,8 +1,34 @@
-
+from repository import InMemoryRepository
+from print_data import PrintData
 
 if __name__ == '__main__':
     # TODO: Create a new instance of InMemoryRepository
+    in_memory_repository = InMemoryRepository()
+
     # TODO: Create a new instance of PrintData
+    print_data_instance = PrintData()
+
     # TODO: Add id, strings... with array format to the repository
+    data_array = [
+        {
+            'id': 1,
+            'name': 'Star Wars'
+        },
+        {
+            'id': 2,
+            'name': 'Star Trek'
+        },
+        {
+            'id': 3,
+            'name': 'The Lord of the Rings'
+        }
+    ]
+    in_memory_repository.add_all(data_array)
+
     # TODO: Delete the movie with id 2
+    in_memory_repository.delete(1)
+
     # TODO: print all the ids
+    all_movies = in_memory_repository.get_all()
+    for movie in all_movies:
+        print("Movie ID:", movie['id'])
--- alumnos/Adri2/Ejercicio Repositorio/basic/tuple.py
+++ alumnos/Adri2/Ejercicio Repositorio/basic/tuple.py
@@ -1,8 +1,42 @@
+from repository import InMemoryRepository
+from print_data import PrintData
+
 if __name__ == '__main__':
     # TODO: Create a new instance of InMemoryRepository
+    in_memory_repository = InMemoryRepository()
+
     # TODO: Create a new instance of PrintData
+    print_data_instance = PrintData()
+
     # TODO: Add array with multiple tuples to the repository
+    movies = [
+        {
+            'id': 1,
+            'name': 'Star Wars'
+        },
+        {
+            'id': 2,
+            'name': 'Star Trek'
+        },
+        {
+            'id': 3,
+            'name': 'The Lord of the Rings'
+        }
+    ]
+    in_memory_repository.add_all(movies)
+
     # TODO: Print the movies
+    all_movies = in_memory_repository.get_all()
+    print_data_instance.print_data(all_movies)
+
     # TODO: Update the movie with id 2
+    movie_update = {
+        'id': 2,
+        'name': 'Star Trek: The Next Generation'
+    }
+    in_memory_repository.update(1, movie_update)
+
     # TODO: Print the movies
+    all_movies_updated = in_memory_repository.get_all()
+    print_data_instance.print_data(all_movies_updated)
 
