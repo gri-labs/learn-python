da319127e6ca4e7b17bdd4591ac9da138a0e85ac
ricardozd <ricardo.olivera@tangelogames.com>
2023-07-20 19:33:20 +0200

    WIP

--- alumnos/Jesus/repository/memory/basic/dicts.py
+++ alumnos/Jesus/repository/memory/basic/dicts.py
@@ -4,6 +4,7 @@ from print_data import PrintData
 if __name__ == '__main__':
     print("Enjoy!")
     in_memory_repository = InMemoryRepository()
+    print_data = PrintData()
 
     # Add dictionary to array
     movie = {
@@ -17,10 +18,7 @@ if __name__ == '__main__':
 
     movie_position_0 = in_memory_repository.get(0)
 
-    for key, value in movie_position_0.items():
-        print("Printing keys and values from movie with id 1:")
-        print("Key: ", key, "Value: ", value)
-        print("Only value: ", value)
+    print_data.print_data(movie_position_0)
 
     # TODO: Add 2 movies to the repository with the following data:
     movies = [
@@ -40,23 +38,17 @@ if __name__ == '__main__':
 
     movie_position_1 = in_memory_repository.get(1)
 
-    for key, value in movie_position_1.items():
-        print(key, value)
+    print_data.print_data(movie_position_1)
 
     # TODO: Print the movie with id 3
 
     movie_position_2 = in_memory_repository.get(2)
 
-    for key, value in movie_position_2.items():
-        print(key, value)
+    print_data.print_data(movie_position_2)
 
     # TODO: Print all the movies
 
     movies = in_memory_repository.get_all()
-
-    for movie in movies:
-        for key, value in movie.items():
-            print(key, value)
 
     # TODO: Update the movie with id 2 with the following data:
 
@@ -71,8 +63,7 @@ if __name__ == '__main__':
 
     movie_position_1_updated = in_memory_repository.get(1)
 
-    for key, value in movie_position_1_updated.items():
-        print(key, value)
+    print_data.print_data(movie_position_1_updated)
 
     # TODO: Delete the movie with id 1
 
@@ -81,20 +72,4 @@ if __name__ == '__main__':
     # TODO: Print all the movies
     result = in_memory_repository.get_all()
 
-    for i in result:
-        print("Final results: ")
-        print("Type: ", type(i))
-        for key, value in i.items():
-            print(key, value)
-
-    # TODO: Print with class PrintMovies
-    # Instanciamos la clase PrintMovies
-    print_movies = PrintData()
-
-    # Llamamos al metodo print_data pasandole como parametro el resultado que es un array
-    print("Printing array with class: ")
-    print_movies.print_data(result)
-
-    # Llamamos al metodo print_data pasandole como parametro el resultado que es un diccionario
-    print("Printing dictionary with class: ")
-    print_movies.print_data(movie_position_1_updated)
+    print_data.print_data(result)
