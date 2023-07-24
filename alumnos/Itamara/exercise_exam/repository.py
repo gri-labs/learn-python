b497e867bebf2050dcd78de8029127fe276457ca
naracasaes <naracasaes@hotmail.com>
2023-07-24 19:38:37 +0200

    Update repository.py

--- alumnos/Itamara/exercise_exam/repository.py
+++ alumnos/Itamara/exercise_exam/repository.py
@@ -9,8 +9,8 @@ class Repository:
     def get_users(self, user_id):
         query = "SELECT * FROM users WHERE id = %s;" % user_id
         result = execute_and_fetchone(query)
-        return self.uers.get(result)
+        return self.connector.get(result)
 
     def insert_user(self, user_id, user_name):
         user = {"id": user_id, "name": user_name}
-        self.users.append(user)
+        self.connector.append(user)
