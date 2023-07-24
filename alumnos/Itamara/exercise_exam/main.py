64ccd3ca799ba7bf59889fca4adb5fd97d68040b
naracasaes <naracasaes@hotmail.com>
2023-07-24 19:11:03 +0200

    Update main.py

--- alumnos/Itamara/exercise_exam/main.py
+++ alumnos/Itamara/exercise_exam/main.py
@@ -9,13 +9,18 @@ connector = ConnectorDatabase()
 
 @app.route('/create/user/<int:user_id>/<string:user_name>', methods=['POST'], endpoint='insert_user')
 def insert_user(user_id, user_name):
-
+    repository.insert_user(user_id,user_name)
     return jsonify('User created'), 201
 
 
 @app.route('/user/<int:user_id>', methods=['GET'], endpoint='get_student')
 def get_user(user_id):
-    return jsonify(""), 200
+    user = repository.get_users(user_id)
+    if user:
+        user_data = "id: {}, name: {}".format(user['id'], user['name'])
+        return jsonify(user_data),200
+    else:
+        jsonify("User not found"), 404
 
 
 if __name__ == '__main__':
