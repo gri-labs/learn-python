c63a969d64853829d236329e2f888daf9d191a77
Ricardo Olivera <ricardoolivera.etpc@gmail.com>
2023-09-05 16:19:17 +0200

    WIP

--- scripts/calculator/main.py
+++ scripts/calculator/main.py
@@ -1,12 +1,14 @@
-from basic.multiply import calculate_multiply
 from basic.div import calculate_div
-from geometry.square import CalculateAreaOfSquare
+from basic.multiply import calculate_multiply
+from basic.sum import calculate_sum
+from conditions.evaluate import evaluate_condition
 from geometry.circle import CalculateAreaOfCircle
-from conditions.contition import evaluate_condition
+from geometry.square import CalculateAreaOfSquare
 
 if __name__ == "__main__":
     print("Multiply: ", calculate_multiply(2, 3))
     print("Div: ", calculate_div(2, 3))
+    print("Sum: ", calculate_sum(2, 3))
     area_square = CalculateAreaOfSquare(2)
     print("Area square: ", area_square.calculate())
     area_circle = CalculateAreaOfCircle(2)
