6350f57a52a0944dc25a9dac1b810773e33f8be7
Ricardo Olivera <ricardoolivera.etpc@gmail.com>
2023-09-14 19:33:01 +0200

    Add games

--- examns/UF2177/main.py
+++ examns/UF2177/main.py
@@ -1,14 +1,17 @@
 from flask import Flask
+from controller import Routes
 from repository import Repository
 from connector import ConnectorDatabase
-from controller import Routes
 
 if __name__ == '__main__':
     app = Flask(__name__)
 
     connector_database = ConnectorDatabase(
         host='localhost',
-
+        user='root',
+        password='root',
+        database='UF2177',
+        port=3306
     )
 
     repository = Repository(connector_database)
---
+++ games/guess/main.py
@@ -0,0 +1,48 @@
+from random import randint
+
+
+class GenerateGuessNumber:
+
+    def __init__(self):
+        self.guess_numer = 0
+
+    def generate(self):
+        self.guess_numer = randint(1, 10)
+        return self.guess_numer
+
+
+class PlayGuessNumber:
+
+    def __init__(self, generate_guess_number):
+        self.generated_guess_number = generate_guess_number
+        self.guess_number = 0
+
+    def play(self):
+        self.guess_number = int(input('Guess a number between 1 and 10: '))
+
+    def is_correct(self):
+        return self.guess_number == self.generated_guess_number.guess_numer
+
+    def is_greater(self):
+        return self.guess_number > self.generated_guess_number.guess_numer
+
+    def is_lower(self):
+        return self.guess_number < self.generated_guess_number.guess_numer
+
+
+if __name__ == '__main__':
+    generated_guess_number = GenerateGuessNumber()
+    generated_guess_number.generate()
+
+    play_guess_number = PlayGuessNumber(generated_guess_number)
+
+    while True:
+        play_guess_number.play()
+
+        if play_guess_number.is_correct():
+            print('You win')
+            break
+        elif play_guess_number.is_greater():
+            print('Lower')
+        elif play_guess_number.is_lower():
+            print('Greater')
