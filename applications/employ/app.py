9ebf98a8c1598523322e38dce7df5b56d2087a37
Ricardo Olivera <ricardoolivera.etpc@gmail.com>
2023-09-22 20:08:00 +0200

    WIP

--- applications/employ/app.py
+++ applications/employ/app.py
@@ -21,5 +21,15 @@ def get_employ(id):
     }), 200
 
 
+@app.route('/employ/<int:id>', methods=['DELETE'])
+def delete_employ(id):
+    try:
+        db.session.query(Employs).filter_by(id=id).delete()
+        db.session.commit()
+        return jsonify('Employ delete successfully'), 200
+    except Exception as e:
+        return jsonify('Error...')
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=6000, debug=True)
